7b394032e64c976a93059ac383dc4c670f346a88
Giovanni Carvalho P <giovannicarvalho2101@gmail.com>
2018-11-14 08:28:12 -0200

    Arrumando a tela de usuario1.0

--- js_style/user.js
+++ js_style/user.js
@@ -1,16 +1,20 @@
 var userclossclass = document.getElementById('usercloss');
 var usuarioinformclass = document.getElementById('Usuarioinform');
+var nomet = document.getElementById('nome');
 
 function openuser() {
     document.getElementById("userside").style.height = "60%";
   	 userclossclass.style.display = "block";
   	 usuarioinformclass.style.display = "block";
+  	 nomet.style.display = "none";
+  	 
 }
 
 function closeuser() {
     document.getElementById("userside").style.height = "5%";
     userclossclass.style.display = "none";
     usuarioinformclass.style.display = "none";
+  	nomet.style.display = "block";
  
 }
 window.onclick = function(event) {
@@ -18,5 +22,7 @@ window.onclick = function(event) {
 		        document.getElementById("userside").style.height = "5%";
 		        userclossclass.style.display = "none";
 		        usuarioinformclass.style.display = "none";
+		        nomet.style.display = "block";
+		        
 		    }
 		}
